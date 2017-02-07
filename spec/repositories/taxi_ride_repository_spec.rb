079d50c6b78d56ad3cf12959a30bc2d64380d442
Marcin Kałamarski <marcin.kalamarski@gmail.com>
2017-02-07 15:51:38 +0100

    tests for daily, weekly stats

--- spec/repositories/taxi_ride_repository_spec.rb
+++ spec/repositories/taxi_ride_repository_spec.rb
@@ -3,32 +3,66 @@ require 'rails_helper'
 RSpec.describe TaxiRides::TaxiRideRepository do
   describe 'queries' do
 
-    context '.monthly stats' do
+    context '.monthly_report' do
 
       before :each do
         create(:taxi_ride)
         create(:taxi_ride)
-        @result = TaxiRides::TaxiRideRepository::monthly_report.to_a
+        @results = TaxiRides::TaxiRideRepository::monthly_report.to_a
       end
 
-      it 'aggregation size' do
-        expect(@result.size).to eq(1)
+      it 'counts results' do
+        expect(@results.size).to eq(1)
       end
 
       it 'has proper sum distance' do
-        expect(@result[0].distance).to eq("2000.00 km")
+        expect(@results[0].distance).to eq("2000.00 km")
       end
 
       it 'has proper avg. distance' do
-        expect(@result[0].avg_distance).to eq("1000.00 km")
+        expect(@results[0].avg_distance).to eq("1000.00 km")
       end
 
       it 'has proper avg. price' do
-        expect(@result[0].avg_price).to eq("100.00 EUR")
+        expect(@results[0].avg_price).to eq("100.00 EUR")
       end
 
-      it 'has proper taxis' do
-        expect(@result[0].taxis).to match(/Taxi [0-9]{1,}, Taxi [0-9]{1,}/)
+      it 'has proper taxis string' do
+        expect(@results[0].taxis).to match(/Taxi [0-9]{1,}, Taxi [0-9]{1,}/)
+      end
+    end
+
+    context '.weekly_stats' do
+
+      before :each do
+        create(:taxi_ride)
+        create(:taxi_ride)
+        @result = TaxiRides::TaxiRideRepository.weekly_stats
+      end
+
+      it 'has proper sum distance' do
+        expect(@result.sum_distance).to eq("2000.00 km")
+      end
+
+      it 'has proper sum price' do
+        expect(@result.sum_price).to eq("200.00 EUR")
+      end
+    end
+
+    context '.daily_stats' do
+
+      before :each do
+        create(:taxi_ride)
+        create(:taxi_ride)
+        @result = TaxiRides::TaxiRideRepository.daily_stats
+      end
+
+      it 'has proper sum distance' do
+        expect(@result.sum_distance).to eq("2000.00 km")
+      end
+
+      it 'has proper sum price' do
+        expect(@result.sum_price).to eq("200.00 EUR")
       end
     end
   end
